99b4dc0e65c623301fbbe86c9b2d18bab8b87ba1
Suzana Suzuki <s.dasilvasuzuki@gmail.com>
2020-09-20 20:50:51 -0300

    Finalizado

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from './Tmdb'
 import MovieRow from './components/MovieRow';
-import FeaturedMovie from './components/FeaturedMovie';
+import FeaturedMovie from './components/Featured';
 import Header from './components/Header';
 
 export default () => {
@@ -65,6 +65,12 @@ export default () => {
         Direitos de imagem para Netlfix <br/>
         Dados pegos do site Themoviedb.org
       </footer>
+
+     {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif" alt="loading "  alt="Carregando" /> 
+        </div>     
+     }
     </div>
   );
 }
--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './FeaturedMovie.css';
+import './style.css';
 
 export default ({item}) => {
 
@@ -8,6 +8,11 @@ export default ({item}) => {
     let genres = [];
     for (let i in item.genres) {
         genres.push(item.genres[i].name);
+    }
+
+    let description = item.overview;
+    if(description.length > 200) {
+        description = description.substring(0, 200) + '...';
     }
 
     return (
@@ -24,7 +29,7 @@ export default ({item}) => {
                        <div className="featured--year">{firstDate.getFullYear()}</div>
                        <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                    </div>
-                   <div className="featured--description">{item.overview}</div>
+                   <div className="featured--description">{description}</div>
                    <div className="featured--buttons">
                         <a href={`/watch/${item.id}`} className="featured--watchbutton">➤ Assistir</a>
                         <a href={`/list/add/${item.id}`}  className="featured--mylistbutton">+ Minha Lista</a>
